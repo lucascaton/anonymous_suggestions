249b6ca8da56e3c30d76ff16f756a8092ff7be55
Lucas Caton <lucascaton@gmail.com>
2014-10-30 21:37:19 +1000

    Including form for suggestions

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 angular.module('app', [])
   .controller('MainCtrl', ['$scope', function($scope) {
-    $scope.posts = [
-      { title: 'post 1', upvotes: 5 },
-      { title: 'post 2', upvotes: 2 },
-      { title: 'post 3', upvotes: 15 },
-      { title: 'post 4', upvotes: 9 },
-      { title: 'post 5', upvotes:  4}
+    $scope.description = '';
+
+    $scope.suggestions = [
+      { description: 'suggestion 1', upvotes: 5 },
+      { description: 'suggestion 2', upvotes: 2 },
+      { description: 'suggestion 3', upvotes: 15 },
+      { description: 'suggestion 4', upvotes: 9 },
+      { description: 'suggestion 5', upvotes:  4}
     ];
+
+    $scope.addSuggestion = function() {
+      if ($scope.description === '') { return; }
+
+      $scope.suggestions.push({
+        description: $scope.description,
+        upvotes: 0
+      });
+
+      $scope.description = '';
+    };
+
+    $scope.incrementUpvotes = function(suggestion) {
+      suggestion.upvotes += 1;
+    };
   }]);
